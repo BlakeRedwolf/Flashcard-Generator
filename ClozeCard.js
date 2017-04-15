2f15a587dad6d182c7b755660883626de86b8d5a
Blake Nelson <Blake Nelson>
2017-04-14 22:28:26 -0700

    App not fully functional, working out a few bugs.

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,50 +1,140 @@
 var inquirer = require("inquirer");
 var fs = require("fs");
+// var flashCards = require("./Flashcards.json");
 
-//need to store arguments into an array, and log them via index.
-// var frontArray = [];
-// var backArray = [];
+switch (process.argv[2]) {
+	case "basic":
+		var frontCard = inquirer.prompt([
+		{
+			type: "input",
+			name: "frontCard",
+			message: "Front card: "
+		},
+		{
+			type: "input",
+			name: "backCard",
+			message: "Back card: "
+		}
+		]).then(function(fandb) {
+			var flashCard = BasicCard(fandb.frontCard, fandb.backCard);
+			flashCard.makeCard();
+		});
+		break;
+		case "cloze":
+			var clozeCard = inquirer.prompt([
+		{
+			type: "input",
+			name: "text",
+			message: "Full card: "
+		},
+		{
+			type: "input",
+			name: "cloze",
+			message: "Hidden text: "
+		}
+		]).then(function(fandb) {
+			var flashCard = ClozeCard(fandb.text, fandb.cloze);
+			flashCard.makeCard();
+		});
+		break;
+		default:
+			console.log("err");
+		}
 
-// Create a BasicCard constructor. It should accept front and back arguments.
-var BasicCard = function() {
-	inquirer.prompt({
-
-	})
+function BasicCard(front, back) {
+	if (this instanceof BasicCard) {
+		this.front = front;
+		this.back = back;
+	}
+	else {
+		BasicCard(front, back);
+	}
 }
 
-var firstPresident = function() {
+function ClozeCard(text, cloze) {
+	if (this instanceof ClozeCard) {
+		this.text = text;
+		this.cloze = cloze;
+		this.hidden = text.replace(cloze, "(____)")
+	}
+	else {
+		console.log("err");
+	}
+}
+
+BasicCard.prototype.makeCard = function() {
+	console.log("frontCard: " + this.front + "\nbackCard: " + this.back);
+}
+
+ClozeCard.prototype.makeCard = function() {
+	if (this.text.included(this.cloze)) {
+		console.log("Success: " + this.hidden);
+	}
+	else {
+		console.log("err");
+	}
+}
+
+//console.log(flashCards);
+// var start = function() {
+// 	if (err) {
+// 		console.log(err);
+// 	}
+// 	inquirer.prompt({
+// 		name: "flashCards",
+// 		message: "Choose [basic] or [cloze]",
+// 		choices: ["basic", "cloze"]
+// 	}).then(function(answer) {
+// 		if (answer.basic) {
+// 			BasicCard();
+// 		}
+// 		else {
+// 			ClozeCard();
+// 		}
+// 	});
+// };
+
+// Create a BasicCard constructor. It should accept front and back arguments.
+// function BasicCard(front, back, cloze) {
+// 	this.front = front;
+// 	this.back = back;
+// 	this.cloze = cloze;
+// }
+
+// var BasicCard = function() {
+// 	inquirer.prompt([{
+// 		name: ,
+// 		message:
+// 	}, {
+// 		name: ,
+// 		message:
+// 	}]).then(function())
+// }
 
 	//front function firstPresident.front take in argF
 
 	//back function takes in argB
-}
 
-var firstPresidentCloze = function() {
 	
 	// ClozeCard function
+// function ClozeCard(textArg, clozeArg, fullArg) {
+// 	this.textArg = textArg;
+// 	this.clozeArg = clozeArg;
+// 	this.fullArg = fullArg;
+// }
 
 	// cloze function ex. 'George Washington'
 
 	// partial function ex. 'was the first president of the United States Of America'
 
-}
-
-var pick = function(caseData, functionData) {
-	switch (caseData) {
-		case 
-	}
-}
+// }
 
 // This take in command line arguments and will execute the correct function
-var inputArgs = function(argF, argB) {
-	pick(argF, argB);
-};
+// var inputArgs = function(argF, argB) {
+// 	pick(argF, argB);
+// };
 
-inputArgs(process.argv[2], process.argv[3]);
-
-
-
-
+// inputArgs(process.argv[2], process.argv[3]);
 
 
 // Create a ClozeCard constructor. It should accept text and cloze arguments.
@@ -55,31 +145,26 @@ inputArgs(process.argv[2], process.argv[3]);
 // ClozeCard should throw or log an error when the cloze deletion does not appear in the input text.
 // Use prototypes to attach these methods, wherever possible.
 
+// var firstPresident = new BasicCard(
+//     "Who was the first president of the United States?", "George Washington");
 
+// // "Who was the first president of the United States?"
+// console.log(firstPresident.front); 
 
+// // "George Washington"
+// console.log(firstPresident.back); 
 
+// var firstPresidentCloze = new ClozeCard(
+//     "George Washington was the first president of the United States.", "George Washington");
 
+// // "George Washington"
+// console.log(firstPresidentCloze.cloze); 
 
-var firstPresident = new BasicCard(
-    "Who was the first president of the United States?", "George Washington");
+// // " ... was the first president of the United States.
+// console.log(firstPresidentCloze.partial); "
 
-// "Who was the first president of the United States?"
-console.log(firstPresident.front); 
+// // "George Washington was the first president of the United States.
+// console.log(firstPresidentCloze.fullText): "
 
-// "George Washington"
-console.log(firstPresident.back); 
-
-var firstPresidentCloze = new ClozeCard(
-    "George Washington was the first president of the United States.", "George Washington");
-
-// "George Washington"
-console.log(firstPresidentCloze.cloze); 
-
-// " ... was the first president of the United States.
-console.log(firstPresidentCloze.partial); "
-
-// "George Washington was the first president of the United States.
-console.log(firstPresidentCloze.fullText): "
-
-// Should throw or log an error because "oops" doesn't appear in "This doesn't work"
-var brokenCloze("This doesn't work", "oops"); 
+// // Should throw or log an error because "oops" doesn't appear in "This doesn't work"
+// var brokenCloze("This doesn't work", "oops"); 
--- README.md
+++ README.md
@@ -1,50 +1,26 @@
 # HW Week 11 - {Flashcard-Generator}
 A basic backend flashcard application that will allow users to create two types of flashcards.
 
-## Live Link (If relevant)
- - www.example.com
+## Live Link
+ - https://github.com/BlakeRedwolf/Flashcard-Generator
 
 ## Description on how to use the app
+Choose either basic or cloze to begin making cards.
 
 ## Requirements
 #### Add a simple description of what the HW requirements were
 
-- Add bullets
-- Like this
+- In this week's assignment, you will create the backend for a basic flashcard application.
+The backend will essentially constitute an API that allows users to create two types of flashcards.
 
-1. Or you can use numbered lists
-2. Like this
-	- You can also add a tab and a "-" to add a sub-bullet like this
-3. Make sure the requirements are understandable
+- Basic flashcards, which have a front ("Who was the first president of the United States?"), and a back ("George Washington").
+
+- Cloze-Deleted flashcards, which present partial text ("... was the first president of the United States."), and the full text when the user requests it ("George Washington was the first president of the United States.")
 
 ## Technologies Used
-#### Use bullets to list out the technologies used. For example,
-- Jquery for Dom Manipulation
-- AJAX for API GET requests
+- 
 
 ## Code Explaination
-- Here, you can either provide a brief summary about your code and perhaps what you learned or you can go into specif detail about how you tackled certain tasks.
-- Use code snippets for placing example code and then describing it
-- Use subheaders to organize your thoughts
-- This is the most important part as it will show other what your code does with out having to download the code. 
-- In essense, this will also be a form of notes that you may later reference weeks later
+-
 
 -------------
-
-##Here is an example of what a Readme could look like:
-
-### AJAX Request to Giphy (Example)
-I created a function that allowed me to make an AJAX request to the Giphy API and then allowed me to pass through a callback function in order to further process the JSON object that was returned. 
-
-```
-var settings = {
-  "url": "http://api.giphy.com/v1/gifs/search?q=funny%20cat&api_key=dc6zaTOxFJmzC",
-  "method": "GET"
-  }
-}
-function getGiphyList(cb){
-	$.ajax(settings).done(function (response) {
-	  cb(response)
-	});
-}
-```
